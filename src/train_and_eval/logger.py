3f1fc7dc6ff28b43def41bf052ea11198a913e16
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2024-09-18 10:46:05 +0700

    add trainer

--- src/train_and_eval/logger.py
+++ src/train_and_eval/logger.py
@@ -3,6 +3,11 @@ import logging
 LOGGER_NAME = "vul_lib_logger"
 
 def _setup_logger():
+    loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+    for log in loggers:
+        if "transformers" in logger.name.lower():
+            log.setLevel(logging.ERROR)
+
     log_format = logging.Formatter("[%(asctime)s %(levelname)s] %(message)s")
     logger = logging.getLogger(LOGGER_NAME)
     logger.setLevel(logging.INFO)
